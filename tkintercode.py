552283d6af738c31eea4138e6e00c5d14aafb54b
josephlbj <josephlbj@gmail.com>
2018-05-16 17:57:09 +0100

    Added some more stuff. Need to add the saving gui :floppy_disk:

--- tkintercode.py
+++ tkintercode.py
@@ -4,9 +4,16 @@ wind2 = tk.Tk()
 wind2.withdraw()
 wind3 = tk.Tk()
 wind3.withdraw()
+wind4 = tk.Tk()
+wind4.withdraw()
+wind5 = tk.Tk()
+wind5.withdraw()
 window.title("Widgets Example")
 use=""
 pas=""
+global alltogether
+alltogether=[]
+total=0
 
 def callback():
     use=username.get()
@@ -16,6 +23,25 @@ def callback():
     login(use,pas)
     username.delete(99999)
     password.delete(99999)
+
+def callback2():
+    global outof2
+    global amountpeople2
+    outof2=outoflbl.get()
+    amountpeople2=amountpeoplelbl.get()
+    print(outof2)
+    print(amountpeople2)
+    marks2(int(amountpeople2),int(outof2))
+    outoflbl.delete(99999)
+    amountpeoplelbl.delete(99999)
+
+def callback3():
+    dhfg=smlbl.get()
+    gdrg=snlbl.get()
+    print(dhfg)
+    print(gdrg)
+    marks3(dhfg,gdrg,outof2)    
+    
 
 def login(lusername,lpassword):
     b=False
@@ -71,41 +97,46 @@ def marks():
     outoflbl.pack()
     tk.Label(wind3,text="How many people?:").pack()
     amountpeoplelbl=tk.Entry(wind3,)
-    submitbtn=tk.Button(wind3)
+    submitbtn=tk.Button(wind3,command=callback2)
     amountpeoplelbl.pack()
+    submitbtn.pack()
     wind3.mainloop()
-    outof=outoflbl.get()
-    amountpeople=amountpeoplelbl.get()
+
+def marks2(amountpeople,outof):
+    global x
+    wind3.destroy()
     x=False
     for i in range(amountpeople):
         x=False
         while x != True:
-            sn=input("Student name: ")
-            sm=int(input("Student mark (out of {})".format(outof)))
-            sp="{}%".format(round(100*(sm/outof)))
-            if sm <= outof and sm >= 0:
-                x=True
-            else:
-                x=False
-        tname.append(sn)
-        tmark.append(sm)
-        tperc.append(sp)
-    for i in range(len(tname)):
-        jsds.append("{}:{}:{}".format(tmark[i],tname[i],tperc[i]))
-    jsds.sort()
-    for i in range(len(tmark)):
-        totals+=tmark[i]
-    total=totals%len(tmark)
-    print("Average mark: {}".format(total))
-    for i in range(len(tname)):
-        print(jsds[i])
+            global smlbl
+            global snlbl
+            wind4.deiconify()
+            #sn=input("Student name: ")
+            tk.Label(wind4,text="Student name:").pack()
+            snlbl=tk.Entry(wind4)
+            snlbl.pack()
+            #sm=int(input("Student mark (out of {})".format(outof)))
+            tk.Label(wind4,text="Student mark:").pack()
+            smlbl=tk.Entry(wind4)
+            smlbl.pack()
+            submitbtn2=tk.Button(wind4,command=callback3)
+            submitbtn2.pack()            
+        
+#    for i in range(len(tname)):
+#        jsds.append("{}:{}:{}".format(tmark[i],tname[i],tperc[i]))
+    alltogether.sort()
+    totala=total%amountpeople
+    print("Average mark: {}".format(totala))
+    for i in range(amountpeople):
+        print(alltogether[i])
         
     hiks=yorn("Would you like to save these marks? ")
     if hiks == True:
         filename=input("Enter a file name: ")
         f=open("{}.txt".format(filename),"a+")
-        for i in range(len(jsds)):
-            f.write("\n{}".format(jsds[i]))
+        for i in range(len(alltogether)):
+            f.write("\n{}".format(alltogether[i]))
         f.close()
         print("Done!")
         print("Saved at \"/{}\"".format(filename))
@@ -113,8 +144,21 @@ def marks():
         print("Ok.")
         init()
     
-    
-    
+def marks3(mark,name,outof):
+    global total
+    if mark <= outof:
+        perc=str((round(mark/outof,1)%100))
+        alltogether.append("{}:{}:{}".format(mark,name,perc))
+        x=True
+        total += mark
+    else:
+        wlbl=tk.Label(wind4,text="Incorrect mark input",bg="#FF0000")
+        wlbl.pack() 
+        x=False       
+        return 
+
+def init():
+    start()    
 loginlbl=tk.Label(window, text="Login")
 ulbl = tk.Label(window, text="Username:")
 username = tk.Entry(window, text="username")
